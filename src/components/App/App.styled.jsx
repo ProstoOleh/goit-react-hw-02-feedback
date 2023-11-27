25bd5ee3524b747ac94ab9a60a1461b186c78a94
ProstoOleh <prosto.alezha@gmail.com>
2023-11-27 15:14:38 +0100

    1

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import { theme } from 'constants';
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 335px;
+  padding: 0 20px;
+  @media (min-width: 375px) {
+    max-width: 375px;
+  }
+  @media (min-width: 768px) {
+    max-width: 768px;
+    padding: 0 32px;
+  }
+  @media (min-width: 1440px) {
+    max-width: 1440px;
+  }
+`;
+
+export const StatisticsTitle = styled.h2`
+  font-size: ${theme.font.large};
+  margin: 50px 0 30px;
+`;
---
+++ src/constants/theme.jsx
@@ -0,0 +1,16 @@
+export const theme = Object.freeze({
+  colors: {
+    primaryFont: '#111111',
+    secondaryFont: '#fafafa',
+    gray: '#999',
+    btnBg: '#3c3cc3',
+  },
+  font: {
+    small: '1.15em',
+    medium: '1.3em',
+    large: '2em',
+  },
+  transition: {
+    transition: '0.3s ease-in',
+  },
+});
